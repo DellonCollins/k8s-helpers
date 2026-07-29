2ccfb6e138b9e9033892eb954185aaad041efb47
DellonCollins <collins.dellon@gmail.com>
2026-07-28 21:02:22 -0400

    added configmaps

--- kubesnapshot.sh
+++ kubesnapshot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-watch -d "printf 'DEPLOYMENTS\n'; kubectl get deployments; printf '\nPODS\n'; kubectl get pods"
+watch -d "printf 'DEPLOYMENTS\n'; kubectl get deployments; printf '\nSERVICES\n'; kubectl get services; printf '\nPODS\n'; kubectl get pods; printf '\nCONFIGMAP\n'; kubectl get configmaps;"
 
